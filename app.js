d2200266353d7c74a730435bc9670f304100c3f8
MAFLIXD <santiago.mafla8@gmail.com>
2024-06-28 21:05:40 -0500

    feat: loops

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
+//Variables:
+
 let numeroSecreto = 8;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 porfavor:");
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = "vez";
 
+while(numeroUsuario != numeroSecreto) {
+        numeroUsuario = prompt("Me indicas un numero entre 1 y 10 porfavor:");
 
-if (numeroUsuario==numeroSecreto) {
-    //Acertamos, fue verdadera la condicion
-    alert(`Acertaste, el numero es: ${numeroUsuario}`)
-}
+        
+    console.log(numeroUsuario);
+        if (numeroUsuario==numeroSecreto) {
+            //Acertamos, fue verdadera la condicion
+            alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces} `)
+        }
 
-else {
-    //La condicion no se cumplio
-    alert("Lo siento,no acertaste el numero");
-}
+        else {
+        
+            if (numeroUsuario>numeroSecreto){
+            alert("El numero secreto es menor");
+        }
+            else {
+                alert("El numero secreto es mayor");
+            }
+
+            intentos = intentos + 1;
+            palabraVeces = "veces";
+            //La condicion no se cumplio
+            //alert("Lo siento,no acertaste el numero");
+        }
+
+    }
