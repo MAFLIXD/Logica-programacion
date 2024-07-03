dc93e87b97ce97ba7a0079825e0f4b0d73a43b74
MAFLIXD <santiago.mafla8@gmail.com>
2024-07-03 15:47:39 -0500

    feat: Programa-final

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //Variables:
 
-let numeroSecreto = 8;
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0;
 let intentos = 1;
 let palabraVeces = "vez";
+let maximosIntentos = 3;
 
 while(numeroUsuario != numeroSecreto) {
         numeroUsuario = prompt("Me indicas un numero entre 1 y 10 porfavor:");
@@ -12,7 +13,7 @@ while(numeroUsuario != numeroSecreto) {
     console.log(numeroUsuario);
         if (numeroUsuario==numeroSecreto) {
             //Acertamos, fue verdadera la condicion
-            alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces} `)
+            alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? "vez" : "veces"} `);
         }
 
         else {
@@ -23,9 +24,13 @@ while(numeroUsuario != numeroSecreto) {
             else {
                 alert("El numero secreto es mayor");
             }
-
-            intentos = intentos + 1;
+            //Incrementamos el comtador cuando no acierta 
+            intentos++;
             palabraVeces = "veces";
+            if(intentos > maximosIntentos){
+                alert(`Llegaste al numero maximo de ${maximosIntentos} intentos`);
+                break; 
+            }
             //La condicion no se cumplio
             //alert("Lo siento,no acertaste el numero");
         }
